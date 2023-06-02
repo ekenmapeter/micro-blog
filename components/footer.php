b41db5e99bb6e30755943002e19b06ad01c434f1
ekenmapeter <ekenma.peter@gmail.com>
2023-06-02 04:07:06 +0100

    Initial commit

---
+++ components/footer.php
@@ -0,0 +1,10 @@
+
+<div class="footer">
+<div id="container">
+<div id="cont">
+<div class="footer_center">
+     <h3>MicroBlog &copy; 2023 ALL Rights Reserved</h3>
+</div>
+</div>
+</div>
+</div>
---
+++ index.php
@@ -0,0 +1,19 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MicroBlog - Get the latest blog news around the world.</title>
+    <link rel="stylesheet" type="text/css" href="css/navbar.css">
+    <link rel="stylesheet" type="text/css" href="css/footer.css">
+    <script src="script.js"></script>
+</head>
+<body>
+    <?php include 'components/navbar.php'; ?>
+
+
+
+    <?php include 'components/footer.php'; ?>
+
+</body>
+</html>
