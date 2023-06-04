7e892f1ebff5f46fc8cc6b53ec1f12e34e15c44c
ekenmapeter <ekenma.peter@gmail.com>
2023-06-04 14:22:26 +0100

    Home Page Blog

--- index.php
+++ index.php
@@ -1,19 +1,231 @@
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>MicroBlog - Get the latest blog news around the world.</title>
-    <link rel="stylesheet" type="text/css" href="css/navbar.css">
-    <link rel="stylesheet" type="text/css" href="css/footer.css">
-    <script src="script.js"></script>
-</head>
-<body>
-    <?php include 'components/navbar.php'; ?>
+    <head>
+        <meta charset="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>MicroBlog - Get the latest blog news around the world.</title>
+        <link rel="stylesheet" type="text/css" href="css/navbar.css" />
+        <link rel="stylesheet" type="text/css" href="css/footer.css" />
+        <link rel="stylesheet" type="text/css" href="css/blog.css" />
+    </head>
+    <body>
+        <?php include 'components/navbar.php'; ?>
 
+        <div class="space-up">
+            <div class="grid-1">
+                <div>
+                    <div style="padding: 4px;">
+                        <p class="blog-section-header">Business News</p>
+                    </div>
+                    <div class="grid" style="padding: 4px;">
+                        <div class="grid-1 border">
+                            <img src="https://source.unsplash.com/random" alt="Random Image" />
+                            <div>
+                                <h3 class="blog-title">This is the first grid item in this section post.</h3>
+                                <a class="read-more" href="#">Read More >></a>
+                            </div>
+                        </div>
+                        <div class="grid-1 border">
+                            <img src="https://source.unsplash.com/random" alt="Random Image" />
+                            <div>
+                                <h3 class="blog-title">This is the first grid item in this section post.</h3>
+                                <a class="read-more" href="#">Read More >></a>
+                            </div>
+                        </div>
+                        <div class="grid-1 border">
+                            <a href="#"><img src="https://source.unsplash.com/random" alt="Random Image" /></a>
+                            <div>
+                                <a href="#"><h3 class="blog-title">This is the first grid item in this section post.</h3></a>
+                                <a class="read-more" href="#">Read More >></a>
+                            </div>
+                        </div>
+                        <div class="grid-1 border">
+                            <a href="#"><img src="https://source.unsplash.com/random" alt="Random Image" /></a>
+                            <div>
+                                <a href="#"><h3 class="blog-title">This is the first grid item in this section post.</h3></a>
+                                <a class="read-more" href="#">Read More >></a>
+                            </div>
+                        </div>
+                        <div class="grid-1 border">
+                            <a href="#"><img src="https://source.unsplash.com/random" alt="Random Image" /></a>
+                            <div>
+                                <a href="#"><h3 class="blog-title">This is the first grid item in this section post.</h3></a>
+                                <a class="read-more" href="#">Read More >></a>
+                            </div>
+                        </div>
+                        <div class="grid-1 border">
+                            <a href="#"><img src="https://source.unsplash.com/random" alt="Random Image" /></a>
+                            <div>
+                                <a href="#"><h3 class="blog-title">This is the first grid item in this section post.</h3></a>
+                                <a class="read-more" href="#">Read More >></a>
+                            </div>
+                        </div>
+                    </div>
+                    <div style="padding: 4px;">
+                        <button class="more-button">More News</button>
+                    </div>
+                </div>
+                <div>
+                    <div style="padding: 4px;">
+                        <p class="blog-section-header">Sport News</p>
+                    </div>
+                    <div class="grid" style="padding: 4px;">
+                        <div class="grid-1 border">
+                            <img src="https://source.unsplash.com/random" alt="Random Image" />
+                            <div>
+                                <h3 class="blog-title">This is the first grid item in this section post.</h3>
+                                <a class="read-more" href="#">Read More >></a>
+                            </div>
+                        </div>
+                        <div class="grid-1 border">
+                            <img src="https://source.unsplash.com/random" alt="Random Image" />
+                            <div>
+                                <h3 class="blog-title">This is the first grid item in this section post.</h3>
+                                <a class="read-more" href="#">Read More >></a>
+                            </div>
+                        </div>
+                        <div class="grid-1 border">
+                            <img src="https://source.unsplash.com/random" alt="Random Image" />
+                            <div>
+                                <h3 class="blog-title">This is the first grid item in this section post.</h3>
+                                <a class="read-more" href="#">Read More >></a>
+                            </div>
+                        </div>
+                        <div class="grid-1 border">
+                            <img src="https://source.unsplash.com/random" alt="Random Image" />
+                            <div>
+                                <h3 class="blog-title">This is the first grid item in this section post.</h3>
+                                <a class="read-more" href="#">Read More >></a>
+                            </div>
+                        </div>
+                        <div class="grid-1 border">
+                            <img src="https://source.unsplash.com/random" alt="Random Image" />
+                            <div>
+                                <h3 class="blog-title">This is the first grid item in this section post.</h3>
+                                <a class="read-more" href="#">Read More >></a>
+                            </div>
+                        </div>
+                        <div class="grid-1 border">
+                            <img src="https://source.unsplash.com/random" alt="Random Image" />
+                            <div>
+                                <h3 class="blog-title">This is the first grid item in this section post.</h3>
+                                <a class="read-more" href="#">Read More >></a>
+                            </div>
+                        </div>
+                    </div>
+                    <div style="padding: 4px;">
+                        <button class="more-button">More News</button>
+                    </div>
+                </div>
+            </div>
+            <div class="grid-1">
+                <div>
+                    <div style="padding: 4px;">
+                        <p class="blog-section-header">Entertainment News</p>
+                    </div>
+                    <div class="grid" style="padding: 4px;">
+                        <div class="grid-1 border">
+                            <a href="#"><img src="https://source.unsplash.com/random" alt="Random Image" /></a>
+                            <div>
+                                <a href="#"><h3 class="blog-title">This is the first grid item in this section post.</h3></a>
+                                <a class="read-more" href="#">Read More >></a>
+                            </div>
+                        </div>
+                        <div class="grid-1 border">
+                            <a href="#"><img src="https://source.unsplash.com/random" alt="Random Image" /></a>
+                            <div>
+                                <a href="#"><h3 class="blog-title">This is the first grid item in this section post.</h3></a>
+                                <a class="read-more" href="#">Read More >></a>
+                            </div>
+                        </div>
+                        <div class="grid-1 border">
+                            <a href="#"><img src="https://source.unsplash.com/random" alt="Random Image" /></a>
+                            <div>
+                                <a href="#"><h3 class="blog-title">This is the first grid item in this section post.</h3></a>
+                                <a class="read-more" href="#">Read More >></a>
+                            </div>
+                        </div>
+                        <div class="grid-1 border">
+                            <a href="#"><img src="https://source.unsplash.com/random" alt="Random Image" /></a>
+                            <div>
+                                <a href="#"><h3 class="blog-title">This is the first grid item in this section post.</h3></a>
+                                <a class="read-more" href="#">Read More >></a>
+                            </div>
+                        </div>
+                        <div class="grid-1 border">
+                            <a href="#"><img src="https://source.unsplash.com/random" alt="Random Image" /></a>
+                            <div>
+                                <a href="#"><h3 class="blog-title">This is the first grid item in this section post.</h3></a>
+                                <a class="read-more" href="#">Read More >></a>
+                            </div>
+                        </div>
+                        <div class="grid-1 border">
+                            <a href="#"><img src="https://source.unsplash.com/random" alt="Random Image" /></a>
+                            <div>
+                                <a href="#"><h3 class="blog-title">This is the first grid item in this section post.</h3></a>
+                                <a class="read-more" href="#">Read More >></a>
+                            </div>
+                        </div>
+                    </div>
+                    <div style="padding: 4px;">
+                        <button class="more-button">More News</button>
+                    </div>
+                </div>
+                <div>
+                    <div style="padding: 4px;">
+                        <p class="blog-section-header">World News</p>
+                    </div>
+                    <div class="grid" style="padding: 4px;">
+                        <div class="grid-1 border">
+                            <a href="#"><img src="https://source.unsplash.com/random" alt="Random Image" /></a>
+                            <div>
+                                <a href="#"><h3 class="blog-title">This is the first grid item in this section post.</h3></a>
+                                <a class="read-more" href="#">Read More >></a>
+                            </div>
+                        </div>
+                        <div class="grid-1 border">
+                            <a href="#"><img src="https://source.unsplash.com/random" alt="Random Image" /></a>
+                            <div>
+                                <a href="#"><h3 class="blog-title">This is the first grid item in this section post.</h3></a>
+                                <a class="read-more" href="#">Read More >></a>
+                            </div>
+                        </div>
+                        <div class="grid-1 border">
+                            <a href="#"><img src="https://source.unsplash.com/random" alt="Random Image" /></a>
+                            <div>
+                                <a href="#"><h3 class="blog-title">This is the first grid item in this section post.</h3></a>
+                                <a class="read-more" href="#">Read More >></a>
+                            </div>
+                        </div>
+                        <div class="grid-1 border">
+                            <a href="#"><img src="https://source.unsplash.com/random" alt="Random Image" /></a>
+                            <div>
+                                <a href="#"><h3 class="blog-title">This is the first grid item in this section post.</h3></a>
+                                <a class="read-more" href="#">Read More >></a>
+                            </div>
+                        </div>
+                        <div class="grid-1 border">
+                            <a href="#"><img src="https://source.unsplash.com/random" alt="Random Image" /></a>
+                            <div>
+                                <a href="#"><h3 class="blog-title">This is the first grid item in this section post.</h3></a>
+                                <a class="read-more" href="#">Read More >></a>
+                            </div>
+                        </div>
+                        <div class="grid-1 border">
+                            <a href="#"><img src="https://source.unsplash.com/random" alt="Random Image" /></a>
+                            <div>
+                                <a href="#"><h3 class="blog-title">This is the first grid item in this section post.</h3></a>
+                                <a class="read-more" href="#">Read More >></a>
+                            </div>
+                        </div>
+                    </div>
+                    <div style="padding: 4px;">
+                        <button class="more-button">More News</button>
+                    </div>
+                </div>
+            </div>
+        </div>
 
-
-    <?php include 'components/footer.php'; ?>
-
-</body>
+        <?php include 'components/footer.php'; ?>
+    </body>
 </html>
